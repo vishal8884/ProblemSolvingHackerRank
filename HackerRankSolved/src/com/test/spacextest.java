a5e968bb7fa162e16067a8a9b1e8a87b830a8b42
vishal <vishalambekar459@gmail.com>
2021-07-22 18:49:11 +0530

    Leet code interview codes updated

--- HackerRankSolved/src/com/test/spacextest.java
+++ HackerRankSolved/src/com/test/spacextest.java
@@ -5,18 +5,121 @@ public class spacextest {
 	public static void main(String[] args) {
 
 		
-		printAsWord(3);
+		printAsWord(382);
 	}
 	
 	
 	
-	public static void printAsWord(int n)
+	public static void printAsWord(int x)
 	{
-	    String num="";  	
-		switch(n)
+        int last=0;
+        String num="";  	
+        int n=rev(x);
+		while(n!=0)
 		{
-		case 0:
+			last=n%10;
+			n=n/10;
+			System.out.println("last :: "+last +"   n :: "+n);
+			
+			switch(last)
+			{
+			case 0:
+				num=num+" zero";
+				break;
+			case 1:
+				num=num+" one";
+				break;
+			case 2:
+				num=num+" two";
+				break;
+			case 3:
+				num=num+" three";
+				break;
+			case 4:
+				num=num+" four";
+				break;
+			case 5:
+				num=num+" five";
+				break;
+			case 6:
+				num=num+" six";
+				break;
+			case 7:
+				num=num+" seven";
+				break;
+			case 8:
+				num=num+" eight";
+				break;
+			case 9:
+				num=num+" nine";	
+				break;
+			}
 		}
+		System.out.println("num :: "+num);
+	}
+	
+	
+	public static int rev(int n)
+	{
+		int digit=0,rev=0;
+		while(n!=0)
+		{
+			digit=n%10;
+			rev=rev*10+digit;
+			n=n/10;
+		}
+		
+		return rev;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//switch(n)
+//{
+//case 0:
+//	num="zero";
+//	break;
+//case 1:
+//	num="one";
+//	break;
+//case 2:
+//	num="two";
+//	break;
+//case 3:
+//	num="three";
+//	break;
+//case 4:
+//	num="four";
+//	break;
+//case 5:
+//	num="five";
+//	break;
+//case 6:
+//	num="six";
+//	break;
+//case 7:
+//	num="seven";
+//	break;
+//case 8:
+//	num="eight";
+//	break;
+//case 9:
+//	num="nine";	
+//	break;
+//}
